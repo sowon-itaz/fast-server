7755fbad2a44571a0c62064eef7a7454ea875889
sowon <sowon@itaz.kr>
2021-08-12 14:25:57 +0900

    post 방식 구현

--- src/main/java/com/example/demo/controller/ServerApiComtroller.java
+++ src/main/java/com/example/demo/controller/ServerApiComtroller.java
@@ -1,12 +1,18 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dto.User;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiComtroller {
@@ -17,5 +23,12 @@ public class ServerApiComtroller {
 		u.setName(name);
 		u.setAge(age);
 		return u;
+	}
+	
+	@PostMapping("/user/{userId}/name/{userName}")
+	public User post(@RequestBody User user, @PathVariable String userId, @PathVariable String userName) {
+		log.info("client req: {}", user);
+		log.info("userId: {}, userName: {}", userId, userName);
+		return user;
 	}
 }
